9714258ca31e2fe1e8ff8dbb9e496b2dcd68c8e4
theprogrammer141 <abdullah.k9913@gmail.com>
2025-06-08 00:34:22 +0500

    add some changes

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   faqContainer.addEventListener("click", (e) => {
     const groupHeader = e.target.closest(".faq-group-header");
-    if (!groupHeader) return;
-
     const group = groupHeader.parentElement;
     const groupBody = group.querySelector(".faq-group-body");
     const icon = groupHeader.querySelector("i");
 
+    if (!groupHeader) return;
     //Toggle icon
     icon.classList.toggle("fa-plus");
     icon.classList.toggle("fa-minus");
@@ -29,14 +28,68 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  const aboutUs = document.getElementById("about-us");
+  const blog = document.getElementById("blog");
+  const faqLis = document.querySelectorAll("li");
+
+  aboutUs.addEventListener("click", () => {
+    document.querySelector(".footer").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  blog.addEventListener("click", () => {
+    document.querySelector(".testimonials").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  faqLis.forEach((li) => {
+    li.addEventListener("click", () => {
+      faqLis.forEach((item) => item.classList.remove("active"));
+
+      li.classList.add("active");
+
+      if (li.id === "faqAll") {
+        document.querySelector(".faq").scrollIntoView({
+          behavior: "smooth",
+        });
+      } else if (li.id === "faqGettingStarted") {
+        document.querySelector(".hero").scrollIntoView({
+          behavior: "smooth",
+        });
+      } else if (li.id === "faqPricing") {
+        document.querySelector(".pricing").scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
 });
 
 // Mobile Menu
 document.addEventListener("DOMContentLoaded", () => {
   const expandingButton = document.querySelector(".expanding-button");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const blogMobile = document.getElementById("blog-mobile");
+  const aboutUsMobile = document.getElementById("about-us-mobile");
 
   expandingButton.addEventListener("click", () =>
     mobileMenu.classList.toggle("active")
   );
+
+  aboutUsMobile.addEventListener("click", () => {
+    document.querySelector(".footer").scrollIntoView({
+      behavior: "smooth",
+    });
+    mobileMenu.classList.remove("active");
+  });
+
+  blogMobile.addEventListener("click", () => {
+    document.querySelector(".testimonials").scrollIntoView({
+      behavior: "smooth",
+    });
+    mobileMenu.classList.remove("active");
+  });
 });
